00ba36a79504d76ff5f04ca3424e831dec3b2f8c
Amanda Zhu <amanda.zhu@tri-ad.global>
2020-10-25 16:02:47 +0900

    feat: add error handling in golang/simple-module

--- golang/simple-module/greetings/greetings.go
+++ golang/simple-module/greetings/greetings.go
@@ -1,11 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Hello returns a greeting for the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+		// If no name is given, return an error message
+		if name == "" {
+			return "", errors.New("empty name")
+		}
+
 		// Return a greeting that embeds the name in a message.
 		// ":=" is a shortcut to declare and initialize a variable in one line.
 		message := fmt.Sprintf("Hi, %v. Welcome!", name)
-		return message
+		return message, nil
 }
--- golang/simple-module/hello/hello.go
+++ golang/simple-module/hello/hello.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greetings message and print it.
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger
+	// Set the log entry prefix and a flag to disable printing
+	// time, source file and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greetings message.
+	message, err := greetings.Hello("")
+	// If an error occurred, print to console and exit.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
